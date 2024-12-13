3e4c3892d9f33e817925231177a8e7ee9a0a92d1
nazarpunk <nazarpunk@gmail.com>
2024-12-13 10:42:14 +0900

    ui reimport

--- Writerside/resources/pack/run.bat
+++ Writerside/resources/pack/run.bat
@@ -1,1 +1,2 @@
-%UjAPILauncher% -ujapi "dev" -as "console" -window -launch "Warcraft" -loadfile "%w3x%"
+rem %UjAPILauncher% -ujapi "dev" -as "console" -window -launch "Warcraft" -loadfile "%w3x%"
+%UjAPILauncher% -ujapi "" -as "" -launch "Warcraft" -loadfile "%w3x%"
